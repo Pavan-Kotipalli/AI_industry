ea2df35fc0947dfaf91d0cff225cf9248a97e905
Pavan-Kotipalli <141723336+Pavan-Kotipalli@users.noreply.github.com>
2023-08-31 11:58:08 +0530

    server.py

--- SE20UARI123_Assignment-1/server.py
+++ SE20UARI123_Assignment-1/server.py
@@ -7,7 +7,7 @@ async def server(websocket, path):
     reply = await websocket.recv()
     print(f"Received reply from client: {reply}")
 
-start_server = websockets.serve(server, "139.59.28.124", 9000)
+start_server = websockets.serve(server, "143.110.242.113", 9000)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
