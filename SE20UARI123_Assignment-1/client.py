bac75b9a3d601b030a48f60a86112e5788a08998
Pavan-Kotipalli <141723336+Pavan-Kotipalli@users.noreply.github.com>
2023-08-31 11:57:06 +0530

    client.py

--- SE20UARI123_Assignment-1/client.py
+++ SE20UARI123_Assignment-1/client.py
@@ -3,7 +3,7 @@ import asyncio
 import websockets
 
 async def client():
-    async with websockets.connect("ws://139.59.28.124:9000") as websocket:
+    async with websockets.connect("ws://143.110.242.113:9000") as websocket:
         message = await websocket.recv()
         print(f"Received message from server: {message}")
         reply = "Hello from the client!"
